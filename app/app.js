c781081df48644c4bd7a9e10b9621aff20dc681f
Derek Hansen <dhansen@nimbleuser.com>
2014-04-19 11:42:52 -0400

    Introduction to Factories

--- app/app.js
+++ app/app.js
@@ -1,14 +1,17 @@
 var bcrApp = angular.module('bcrApp', []);
 
-bcrApp.controller('SessionsCtrl', function SessionsCtrl($scope) {
-  $scope.sessions = [
-    {
-      "id": 0,
-      "name": "Keynote"
-    },
-    {
-      "id": 1,
-      "name": "Intro to Angular"
+bcrApp.controller('SessionsCtrl', function SessionsCtrl($scope, sessionApi) {
+  sessionApi.getAll().then(function(response) {
+    $scope.sessions = response.data;
+  });
+});
+
+bcrApp.factory('sessionApi', function sessionApi($http) {
+  var api = {
+    getAll: function() {
+      return $http.get('/data/sessions.json');
     }
-  ];
+  };
+
+  return api;
 });
